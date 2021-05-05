6135be384003288aaeeeb2d1c5075b7a94035b9e
berkerdemoglu <berkerdemoglu1120@gmail.com>
2021-05-05 11:10:36 +0300

    use engine constants in projector

--- src/engine/geometry/Projector.java
+++ src/engine/geometry/Projector.java
@@ -1,15 +1,16 @@
 package engine.geometry;
 
-import engine.graphics.Display;
+import engine.Engine;
 
 import java.awt.Point;
 
 public abstract class Projector {
-	private static final int d = 1;
+	private static int d = 1; // distance to objects
 
 	public static Point project3DPoint(Point3D p3d) {
-		int x2d = (int) (Display.SCREEN_WIDTH / 2 + d*p3d.x / p3d.z);
-		int y2d = (int) (Display.SCREEN_HEIGHT / 2 - d*p3d.y / p3d.z);
+		// Add half the screen width and height so that the point is centered in the middle
+		int x2d = (int) (Engine.SCREEN_WIDTH / 2 + d*p3d.x / p3d.z);
+		int y2d = (int) (Engine.SCREEN_HEIGHT / 2 - d*p3d.y / p3d.z);
 
 		return new Point(x2d, y2d);
 	}
