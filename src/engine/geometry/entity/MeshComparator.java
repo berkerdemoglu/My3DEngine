a23e35ac37a9ae137db23e31a37dd02526e42439
berkerdemoglu <berkerdemoglu1120@gmail.com>
2021-05-11 13:58:06 +0300

    Fix mesh comparator not returning 0
    Fixed the mesh comparator's compare method not returning 0 for equal elements, which creates problems and has created problems in the past in the sorting method for lists (TimSort)

--- src/engine/geometry/entity/MeshComparator.java
+++ src/engine/geometry/entity/MeshComparator.java
@@ -10,6 +10,8 @@ import java.util.Comparator;
 public class MeshComparator implements Comparator<Mesh> {
 	@Override
 	public int compare(Mesh m1, Mesh m2) {
+		if (m2.getAverageZ() == m1.getAverageZ()) return 0;
+
 		return m2.getAverageZ() - m1.getAverageZ() < 0 ? 1 : -1;
 	}
 }
